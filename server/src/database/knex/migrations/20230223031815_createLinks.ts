6c8acb0e33347abb5f784b3e7164c5385fc6c4b1
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-02-23 21:44:06 +0000

    feat: Updated Migrations

--- server/src/database/knex/migrations/20230223031815_createLinks.ts
+++ server/src/database/knex/migrations/20230223031815_createLinks.ts
@@ -3,9 +3,9 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("links", (table) => {
     table.increments("id");
-    table.integer("note_id").references("id").inTable("notes");
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
     table.text("url");
-    table.timestamp("created_at").defaultTo(knex.fn.now);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
   });
 }
 
