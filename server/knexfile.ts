a83c2c3e0ba2f374527d55bc52178a3a389603bb
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-02-23 03:46:04 +0000

    fix: Configuring Knex.js

---
+++ server/knexfile.ts
@@ -0,0 +1,22 @@
+import path from "path";
+export default {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db"),
+    },
+    pool: {
+      afterCreate: (connection: any, callback: any) =>
+        connection.run("PRAGMA foreign_keys = ON", callback),
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+  },
+};
