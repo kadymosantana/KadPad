c22d5996b0b63061c26c077170016b868f8fffd9
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-02-24 23:42:31 +0000

    feat: Interfaces Note, Tag And Link Added

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -7,6 +7,25 @@ export interface User {
   updated_at: string;
 }
 
+export interface Note {
+  id: number;
+  user_id: number;
+  title: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
 
+export interface Tag {
+  id: number;
+  note_id: number;
+  user_id: number;
+  name: number;
+}
 
-
+export interface Link {
+  id: number;
+  note_id: number;
+  url: string;
+  created_at: string;
+}
