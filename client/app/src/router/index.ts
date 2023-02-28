16b84d5ccb91cc0e4fe77c493089c66fa82a4635
Kádymo Santana <santanakadymo@gmail.com>
2023-02-28 03:53:22 +0000

    feat: Creating Route to Home

--- client/app/src/router/index.ts
+++ client/app/src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [{ name: 'Home', path: '/', component: Home }]
 })
 
 export default router
