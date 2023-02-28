90486ca05917533c397528e2366c1ef5d8bfd91d
Kádymo Santana <santanakadymo@gmail.com>
2023-02-28 03:54:03 +0000

    feat: Creating the Home Page Component

---
+++ client/app/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue'
+import NotesContainer from '@/components/NotesContainer.vue'
+
+import store from '../store'
+</script>
+
+<template>
+  <div class="text-gray-300 tracking-tight grid grid-cols-[auto_1fr] w-screen min-h-screen">
+    <Sidebar />
+    <NotesContainer />
+  </div>
+</template>
