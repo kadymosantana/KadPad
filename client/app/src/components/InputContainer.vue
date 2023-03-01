adf22db1afc86c0cfc6a5c61319eb85203fb748e
Kádymo Santana <santanakadymo@gmail.com>
2023-03-01 01:56:35 +0000

    feat: Creating Component To Style Text Input

---
+++ client/app/src/components/InputContainer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  type: { type: String, required: false },
+  icon: { type: String, required: true },
+  placeholder: { type: String, required: true }
+})
+</script>
+
+<template>
+  <div
+    :class="{ 'px-6 py-4': type === 'large' }"
+    class="input-container flex border-2 border-solid border-transparent focus-within:border-cyan-500 gap-5 p-3 bg-dark-700 shadow-md rounded-2xl"
+  >
+    <img :src="'src/assets/icons/' + icon + '.svg'" />
+    <input
+      class="bg-transparent placeholder:text-dark-500 w-full"
+      type="text"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
--- client/app/src/components/NotesContainer.vue
+++ client/app/src/components/NotesContainer.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
 import NotesList from './NotesList.vue'
+import InputContainer from './InputContainer.vue'
 </script>
 
 <template>
   <main class="bg-dark-800 p-12 flex flex-col gap-12 ml-[320px]">
-    <div class="flex gap-5 bg-dark-700 rounded-2xl shadow-sm px-6 py-4">
-      <img src="../assets/icons/search.svg" />
-      <input
-        class="text-lg tracking-tight w-full bg-transparent placeholder:text-[#454851]"
-        type="text"
-        placeholder="Pesquisar nota"
-      />
-    </div>
+    <InputContainer type="large" icon="search" placeholder="Pesquisar nota" />
     <NotesList />
   </main>
 </template>
