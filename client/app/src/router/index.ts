82152d06cb4064f316185844eb88c6bc4db4681c
Kádymo Santana <santanakadymo@gmail.com>
2023-03-03 00:42:16 +0000

    feat: Route Created for Login Page

--- client/app/src/router/index.ts
+++ client/app/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/views/Login.vue'
 import Home from '@/views/Home.vue'
 import Profile from '@/views/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { name: 'Home', path: '/', component: Home },
+    { name: 'Login', path: '/', component: Login },
+    { name: 'Home', path: '/home', component: Home },
     { name: 'Profile', path: '/profile', component: Profile }
   ]
 })
