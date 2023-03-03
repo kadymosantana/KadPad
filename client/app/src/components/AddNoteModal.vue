0725f82cc2558d55cb0453517e95557f342ea8b1
Kádymo Santana <santanakadymo@gmail.com>
2023-03-03 20:17:22 +0000

    refactor: Using Components for Links and Tags

--- client/app/src/components/AddNoteModal.vue
+++ client/app/src/components/AddNoteModal.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import store from '../store'
+import NoteItem from './NoteItem.vue'
+import AddButton from './AddButton.vue'
 </script>
 
 <template>
   <Transition name="modal">
     <div v-if="store.addNoteModalState" class="modal-wrapper">
-      <div class="modal-container">
+      <div class="modal-container overflow-auto max-w-screen">
         <header
           class="flex items-center justify-between border-b border-solid border-dark-600 pb-4"
         >
@@ -32,20 +34,10 @@ import store from '../store'
             <img src="../assets/icons/link.svg" alt="" />
             Links
           </h3>
-          <ul class="flex flex-col gap-2 max-h-24 overflow-auto mb-3">
-            <li
-              class="flex justify-between items-center bg-dark-900 p-3 rounded-xl border border-transparent"
-            >
-              <span>https://github.com/kadymosantana</span>
-              <img class="cursor-pointer" src="../assets/icons/close.svg" alt="Remover link" />
-            </li>
+          <ul class="flex flex-col gap-2 max-h-20 overflow-auto mb-3">
+            <NoteItem type="link" value="https://google.com" icon="close" />
           </ul>
-          <button
-            class="flex justify-between items-center border border-dashed border-cyan-500 p-3 w-full rounded-xl"
-          >
-            <span>Novo link</span>
-            <img src="../assets/icons/add-link.svg" />
-          </button>
+          <AddButton icon="add-link" placeholder="Novo link" />
         </div>
 
         <div>
@@ -53,20 +45,10 @@ import store from '../store'
             <img src="../assets/icons/tag.svg" />
             Tags
           </h3>
-          <ul class="flex flex-wrap gap-2 overflow-auto">
-            <li
-              class="flex gap-2 items-center min-w-max bg-dark-900 px-3 py-2 rounded-xl border border-transparent"
-            >
-              <span>vuejs</span>
-              <img src="../assets/icons/close.svg" alt="Remover link" />
-            </li>
-            <button
-              class="flex gap-4 items-center border border-dashed border-cyan-500 px-3 rounded-xl"
-            >
-              <span>Nova tag</span>
-              <img src="../assets/icons/add-tag.svg" />
-            </button>
+          <ul class="flex gap-2 overflow-auto mb-2">
+            <NoteItem type="tag" value="vuejs" icon="close" />
           </ul>
+          <AddButton icon="add-tag" placeholder="Nova tag" />
         </div>
         <button
           class="w-full bg-cyan-500 hover:bg-cyan-600 rounded-xl text-dark-800 p-3 duration-500"
