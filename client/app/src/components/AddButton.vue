3fd9e9d5fb93dbda8a06e348401bc8b1a4139655
Kádymo Santana <santanakadymo@gmail.com>
2023-03-03 20:16:15 +0000

    feat: Created Component Structure And Style

---
+++ client/app/src/components/AddButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+  icon: { type: String, required: true },
+  placeholder: { type: String, required: true }
+})
+</script>
+
+<template>
+  <span
+    class="flex justify-between items-center px-3 py-2 rounded-xl border border-dashed border-dark-500"
+  >
+    <input class="bg-transparent" :placeholder="placeholder" />
+    <button>
+      <img :src="'/src/assets/icons/' + icon + '.svg'" />
+    </button>
+  </span>
+</template>
---
+++ client/app/src/components/NoteItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+  icon: { type: String, required: true }
+})
+</script>
+
+<template>
+  <li
+    class="flex justify-between items-center bg-dark-900 px-3 py-2 rounded-xl border border-transparent"
+    :class="{ underline: type === 'link', 'gap-2 min-w-max': type === 'tag' }"
+  >
+    <a v-if="type === 'link'" class="bg-transparent">{{ value }}</a>
+    <span v-else class="bg-transparent">{{ value }}</span>
+
+    <img class="cursor-pointer" :src="'src/assets/icons/' + icon + '.svg'" alt="" />
+  </li>
+</template>
