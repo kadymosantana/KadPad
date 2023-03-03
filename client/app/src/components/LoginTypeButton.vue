62edc4fefaf46aae55e590c5250c31d293efeac8
Kádymo Santana <santanakadymo@gmail.com>
2023-03-03 00:40:59 +0000

    feat: Created Component Structure And Style

---
+++ client/app/src/components/LoginTypeButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+  activeLoginType: { type: String, required: true },
+  type: { type: String, required: true }
+})
+
+const emits = defineEmits(['change-login-type'])
+</script>
+
+<template>
+  <button
+    @click="$emit('change-login-type')"
+    :class="{ active: activeLoginType === type }"
+    class="relative flex flex-col items-center gap-2 text-2xl font-light"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.active::after {
+  content: '';
+  width: 100%;
+  height: 5px;
+  background-color: #06b6d4;
+  border-radius: 25px;
+  margin-bottom: -3px;
+}
+</style>
---
+++ client/app/src/components/NoteModal.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import store from '../store'
+import Tag from './Tag.vue'
+</script>
+
+<template>
+  <Transition name="modal">
+    <div v-if="store.noteModalState" class="modal-wrapper">
+      <div class="modal-container">
+        <header class="flex justify-between pb-4">
+          <h1 class="text-4xl font-bold">Fundamentos do Vue.js</h1>
+          <button
+            @click="store.noteModalState = false"
+            class="bg-[#ff00001a] p-2 rounded-xl self-start"
+          >
+            <img src="../assets/icons/close.svg" alt="Fechar" />
+          </button>
+        </header>
+
+        <div>
+          <h3 class="subtitle">
+            <img src="../assets/icons/description.svg" />
+            Descrição
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nemo quaerat
+            necessitatibus a delectus excepturi cupiditate quae magni quidem impedit earum qui,
+            neque corrupti odio tenetur quod commodi voluptatem vero. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Placeat odit beatae in nisi rem ipsa quisquam. Quaerat
+            officiis optio incidunt odio. Quas mollitia autem officiis pariatur maxime deleniti
+            consequuntur repellat?
+          </p>
+        </div>
+
+        <div>
+          <h3 class="subtitle">
+            <img src="../assets/icons/link.svg" alt="" />
+            Links
+          </h3>
+          <ul class="flex flex-col gap-2 max-h-20 overflow-auto mb-3">
+            <li class="bg-[#06b6d424] p-2 underline text-cyan-500 rounded-xl">
+              <a href="https://github.com/kadymosantana">https://github.com/kadymosantana</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="subtitle">
+            <img src="../assets/icons/tag.svg" />
+            Tags
+          </h3>
+          <ul class="flex flex-wrap gap-2 mb-3">
+            <Tag />
+            <Tag />
+            <Tag />
+          </ul>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
