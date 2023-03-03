5b6b26f67f91b312d7c49a6cfcff0f5fe77d653b
Kádymo Santana <santanakadymo@gmail.com>
2023-03-03 00:38:10 +0000

    feat: Moving Animation From Modal To "App"

--- client/app/src/App.vue
+++ client/app/src/App.vue
@@ -3,3 +3,19 @@
     <RouterView />
   </div>
 </template>
+
+<style>
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(0);
+  transform: scale(0);
+}
+</style>
