912ba0576eeaceca106001ed8314273dd7ab442d
Kádymo Santana <santanakadymo@gmail.com>
2023-03-03 00:39:26 +0000

    feat: Creating Prop for Input Type

--- client/app/src/components/InputContainer.vue
+++ client/app/src/components/InputContainer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const props = defineProps({
-  type: { type: String, required: false },
+  type: { type: String, default: 'text', required: false },
+  size: { type: String, required: false },
   icon: { type: String, required: true },
   placeholder: { type: String, required: true }
 })
@@ -8,13 +9,13 @@ const props = defineProps({
 
 <template>
   <div
-    :class="{ 'px-6 py-4': type === 'large' }"
-    class="input-container flex border-2 border-solid border-transparent focus-within:border-cyan-500 gap-5 p-3 bg-dark-700 shadow-md rounded-2xl"
+    :class="{ 'px-6 py-4': size === 'large' }"
+    class="input-container flex border-2 border-solid border-transparent focus-within:border-cyan-500 gap-5 p-3 bg-dark-700 shadow-md rounded-2xl duration-500"
   >
     <img :src="'src/assets/icons/' + icon + '.svg'" />
     <input
       class="bg-transparent placeholder:text-dark-500 w-full"
-      type="text"
+      :type="type"
       :placeholder="placeholder"
     />
   </div>
