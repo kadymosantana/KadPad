375959a46ecbb0dbcfe28f0d7abd6f73edb0cd60
Kádymo Santana <santanakadymo@gmail.com>
2023-03-06 23:44:49 +0000

    fix: Solving a Problem Connecting to the Database

--- server/src/database/sqlite/index.ts
+++ server/src/database/sqlite/index.ts
@@ -1,9 +1,9 @@
-import sqlite from "sqlite";
+import { open } from "sqlite";
 import sqlite3 from "sqlite3";
 const path = require("path");
 
 export default async function sqliteConnection() {
-  const database = await sqlite?.open({
+  const database = await open({
     filename: path.resolve(__dirname, "..", "database.db"),
     driver: sqlite3.Database,
   });
