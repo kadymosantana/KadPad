74b38c256663a90440e745a578aa39cee0e7e346
Kádymo Santana <santanakadymo@gmail.com>
2023-03-07 19:36:48 +0000

    feat: Creating Token for User

--- server/src/controllers/SessionsController.ts
+++ server/src/controllers/SessionsController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 const { compare } = require("bcrypt");
 
 import { User } from "../types/index";
 import knex from "../database/knex";
+import authConfig from "../config/auth";
 import AppError from "../../utils/AppError";
 
 export default class SessionsController {
@@ -15,6 +17,9 @@ export default class SessionsController {
     const passwordCheck = await compare(password, user.password);
     if (!passwordCheck) throw new AppError("E-mail e/ou senha inválida.", 401);
 
-    res.json({ user });
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = jwt.sign({}, secret, { subject: String(user.id), expiresIn });
+
+    res.json({ user, token });
   }
 }
