d2bf66199c8aab825d07f1ece9479e6d8c6cefa0
Kádymo Santana <santanakadymo@gmail.com>
2023-03-07 19:53:11 +0000

    feat: Creating Route to Show Note

--- client/app/src/router/index.ts
+++ client/app/src/router/index.ts
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login.vue'
 import Home from '@/views/Home.vue'
 import Profile from '@/views/Profile.vue'
+import NoteModal from '@/components/NoteModal.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { name: 'Login', path: '/', component: Login },
     { name: 'Home', path: '/home', component: Home },
-    { name: 'Profile', path: '/profile', component: Profile }
+    { name: 'Profile', path: '/profile', component: Profile },
+    { name: 'Note', path: '/note', component: NoteModal, meta: { transition: 'modal' } }
   ]
 })
 
