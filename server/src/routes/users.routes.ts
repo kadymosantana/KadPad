6eb7dbdc26bc4ddd0e75b6c31641d8628840b698
Kádymo Santana <santanakadymo@gmail.com>
2023-03-08 00:35:59 +0000

    feat: Creating Route to Upload Avatar

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,12 +1,27 @@
+import { Request, Response } from "express";
 import { Router } from "express";
-import UsersController from "../controllers/UsersController";
+import multer from "multer";
 
+import UsersController from "../controllers/UsersController";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+
+import uploadConfig from "../config/upload";
 
 const usersRoutes = Router();
 const usersController = new UsersController();
 
+const upload = multer(uploadConfig.MULTER);
+
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  (req: Request, res: Response) => {
+    console.log(req.file?.filename);
+    res.status(201);
+  }
+);
 
 module.exports = usersRoutes;
