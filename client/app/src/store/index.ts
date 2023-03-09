4b535eb54553b593a404d51f2bf64788467b7c97
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-09 02:25:38 +0000

    feat: Creating Global State for User Auth Data

--- client/app/src/store/index.ts
+++ client/app/src/store/index.ts
@@ -1,6 +1,8 @@
 import { reactive } from 'vue'
+import type { Store } from '../types'
 
-const store = reactive({
+const store = reactive<Store>({
+  userAuthData: null,
   noteModalState: false,
   addNoteModalState: false
 })
