f2b8a31b46db708408bb0d891e010cd9f3920656
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-09 23:49:59 +0000

    feat: Creating Update User Functionality

--- client/app/src/views/Profile.vue
+++ client/app/src/views/Profile.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 import store from '@/store'
+
+import { api } from '@/services/api'
+
 import InputContainer from '@/components/InputContainer.vue'
 
 const router = useRouter()
-const signOut = () => {
+const toast = useToast()
+
+const name = ref(store.userAuthData!.user.name)
+const email = ref(store.userAuthData!.user.email)
+const oldPassword = ref('')
+const newPassword = ref('')
+
+const handleUpdate = async () => {
+  const updatedUser = {
+    name: name.value,
+    email: email.value,
+    password: newPassword.value,
+    old_password: oldPassword.value
+  }
+  try {
+    await api.put('/users', updatedUser)
+
+    store.userAuthData!.user = { name: name.value, email: email.value }
+    localStorage.setItem('@KadPad:user', JSON.stringify(updatedUser))
+
+    toast.success('Perfil atualizado com sucesso!')
+    oldPassword.value = ''
+    newPassword.value = ''
+  } catch (error: any) {
+    if (error.response) return toast.error(error.response.data.message)
+    else return toast.error('Não foi possível fazer o login.')
+  }
+}
+
+const handleSignOut = () => {
   localStorage.removeItem('@KadPad:user')
   localStorage.removeItem('@KadPad:token')
   store.userAuthData = null
@@ -27,14 +62,24 @@ const signOut = () => {
         ><img src="../assets/icons/add-photo.svg" alt="Adicionar foto"
       /></span>
     </div>
-    <form class="flex flex-col gap-3">
-      <InputContainer icon="user" placeholder="Nome" />
-      <InputContainer type="email" icon="email" placeholder="E-mail" />
-      <InputContainer type="password" icon="password" placeholder="Senha atual" />
-      <InputContainer type="password" icon="password" placeholder="Nova senha" />
+    <form @submit.prevent="handleUpdate" class="flex flex-col gap-3">
+      <InputContainer v-model="name" icon="user" placeholder="Nome" />
+      <InputContainer v-model="email" type="email" icon="email" placeholder="E-mail" />
+      <InputContainer
+        v-model="oldPassword"
+        type="password"
+        icon="password"
+        placeholder="Senha atual"
+      />
+      <InputContainer
+        v-model="newPassword"
+        type="password"
+        icon="password"
+        placeholder="Nova senha"
+      />
       <button class="primary-button mt-3">Salvar</button>
     </form>
-    <button @click="signOut" class="flex items-center gap-2 bg-[#ff00001a] p-3 rounded-xl">
+    <button @click="handleSignOut" class="flex items-center gap-2 bg-[#ff00001a] p-3 rounded-xl">
       <img src="../assets/icons/logout.svg" />
       <span class="text-red-700">Sair</span>
     </button>
