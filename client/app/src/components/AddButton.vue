667c45415993c851f9640d487d0a9ddf782309fc
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-11 01:38:40 +0000

    feat: Emitting Add Item Event

--- client/app/src/components/AddButton.vue
+++ client/app/src/components/AddButton.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 const props = defineProps({
+  modelValue: { type: String },
   icon: { type: String, required: true },
   placeholder: { type: String, required: true }
 })
+const emits = defineEmits(['add', 'update:modelValue'])
 </script>
 
 <template>
   <span
     class="flex justify-between items-center px-3 py-2 rounded-xl border border-dashed border-dark-500"
   >
-    <input class="bg-transparent" :placeholder="placeholder" />
-    <button>
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="bg-transparent"
+      :placeholder="placeholder"
+    />
+    <button @click="$emit('add')">
       <img :src="'/src/assets/icons/' + icon + '.svg'" />
     </button>
   </span>
