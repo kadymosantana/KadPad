20d7c18baf82408600d7d92645e8eae42d9ccab3
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-12 02:57:19 +0000

    feat: Linking To The Route "/new-note"

--- client/app/src/components/NotesList.vue
+++ client/app/src/components/NotesList.vue
@@ -1,42 +1,36 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
 import AddNoteModal from './AddNoteModal.vue'
 import NoteCard from './NoteCard.vue'
-import store from '../store'
+
+const router = useRouter()
 </script>
 
 <template>
   <section>
     <header class="flex items-center justify-between border-b border-solid border-dark-600 pb-4">
       <h2 class="text-3xl font-light">Minhas notas</h2>
-      <button
-        @click="store.addNoteModalState = true"
+      <RouterLink
+        :to="{ name: 'New Note' }"
+        tag="button"
         class="flex gap-3 items-center bg-cyan-500 hover:bg-cyan-600 p-2 rounded-xl duration-500"
       >
         <img src="../assets/icons/add.svg" />
         <span class="text-dark-800 text-lg">Nova nota</span>
-      </button>
+      </RouterLink>
     </header>
     <ul class="pt-8 columns-3">
       <NoteCard> Uma linha. </NoteCard>
-      <NoteCard>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente natus distinctio libero...
-      </NoteCard>
-      <NoteCard>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio expedita, doloremque...
-      </NoteCard>
-      <NoteCard>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam ratione aperiam, voluptatum
-        ut temporibus...
-      </NoteCard>
-      <NoteCard>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam ratione aperiam,
-        voluptatum...
-      </NoteCard>
     </ul>
 
     <Teleport to="body">
-      <AddNoteModal />
+      <RouterView v-slot="{ Component }">
+        <transition name="modal">
+          <component :is="Component" />
+        </transition>
+      </RouterView>
     </Teleport>
   </section>
 </template>
