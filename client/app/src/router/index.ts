c4b6345735116d84ed6a8c51019f676fc76a325f
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-12 02:57:58 +0000

    feat: Creating Route to Add Note

--- client/app/src/router/index.ts
+++ client/app/src/router/index.ts
@@ -6,6 +6,7 @@ import Login from '@/views/Login.vue'
 import Home from '@/views/Home.vue'
 import Profile from '@/views/Profile.vue'
 import NoteModal from '@/components/NoteModal.vue'
+import AddNoteModal from '@/components/AddNoteModal.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,8 @@ const router = createRouter({
     { name: 'Login', path: '/', component: Login },
     { name: 'Home', path: '/home', component: Home },
     { name: 'Profile', path: '/profile', component: Profile },
-    { name: 'Note', path: '/note', component: NoteModal, meta: { transition: 'modal' } }
+    { name: 'Note', path: '/note', component: NoteModal, meta: { transition: 'modal' } },
+    { name: 'New Note', path: '/new-note', component: AddNoteModal, meta: { transition: 'modal' } }
   ]
 })
 
