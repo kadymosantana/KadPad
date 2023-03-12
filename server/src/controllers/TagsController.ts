039f7d5cad5f1fde7734d071db6962c95faac37c
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-12 02:59:35 +0000

    feat: Avoiding the Display of Repeated Tags

--- server/src/controllers/TagsController.ts
+++ server/src/controllers/TagsController.ts
@@ -4,7 +4,9 @@ import knex from "../database/knex";
 export default class TagsController {
   async index(req: Request, res: Response) {
     const user_id = req.user!.id;
-    const userTags = await knex("tags").where("user_id", user_id);
+    const userTags = await knex("tags")
+      .where("user_id", user_id)
+      .groupBy("name");
 
     res.json(userTags);
   }
