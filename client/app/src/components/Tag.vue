719b1c39632f19744cf9a4ecd47956ad44f0c0a1
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-13 00:22:14 +0000

    feat: Displaying text dynamically

--- client/app/src/components/Tag.vue
+++ client/app/src/components/Tag.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: { type: String, required: true }
+})
+</script>
+
 <template>
-  <li class="bg-[#06b6d424] px-2 py-1 text-cyan-500 rounded-xl">vuejs</li>
+  <li class="bg-[#06b6d424] px-2 py-1 text-cyan-500 rounded-xl">{{ name }}</li>
 </template>
