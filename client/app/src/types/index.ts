872dbfaac00de3e3b43461cf8ac17b785f66fb8e
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-13 00:23:14 +0000

    Creating User, Note and Link interfaces

--- client/app/src/types/index.ts
+++ client/app/src/types/index.ts
@@ -1,3 +1,31 @@
+export interface User {
+  id: number
+  name: string
+  email: string
+  password: string
+  avatar: string
+  created_at: string
+  updated_at: string
+}
+
+export interface Note {
+  id: number
+  user_id: number
+  title: string
+  description: string
+  tags: Tag[]
+  links: Link[]
+  created_at: string
+  updated_at: string
+}
+
+export interface Link {
+  id: number
+  note_id: number
+  url: string
+  created_at: string
+}
+
 export interface Tag {
   id: number
   note_id: number
