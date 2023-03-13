c06d86da786e808bd3fcef239607aa8de916582c
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-13 00:19:24 +0000

    feat: State of the search input for the store

--- client/app/src/components/NotesContainer.vue
+++ client/app/src/components/NotesContainer.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+import store from '@/store'
+
 import NotesList from './NotesList.vue'
 import InputContainer from './InputContainer.vue'
+
+const title = ref('')
+watch(title, (value) => {
+  store.searchedNote = value
+})
 </script>
 
 <template>
   <main class="bg-dark-800 p-12 flex flex-col gap-12 ml-[320px]">
-    <InputContainer size="large" icon="search" placeholder="Pesquisar nota" />
+    <InputContainer v-model="title" size="large" icon="search" placeholder="Pesquisar nota" />
     <NotesList />
   </main>
 </template>
