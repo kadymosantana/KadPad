39a0751aa8b7a51d9d5d26a28cef1231188ea09c
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-13 01:03:20 +0000

    feat: Fetching and displaying note details

--- client/app/src/components/NoteModal.vue
+++ client/app/src/components/NoteModal.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import type { Note } from '@/types'
+
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { api } from '@/services/api'
 import store from '../store'
+
 import Tag from './Tag.vue'
 
 const router = useRouter()
+const route = useRoute()
+
+const note = ref<Note | null>(null)
+
+onMounted(() => {
+  fetchNoteDetails()
+})
+
+const fetchNoteDetails = async () => {
+  const noteDetails = await api.get(`/notes/${route.params.id}`)
+  note.value = noteDetails.data
+}
 </script>
 
 <template>
@@ -11,7 +28,7 @@ const router = useRouter()
     <div class="modal-wrapper">
       <div class="modal-container">
         <header class="flex justify-between pb-4">
-          <h1 class="text-4xl font-bold">Fundamentos do Vue.js</h1>
+          <h1 class="text-4xl font-bold">{{ note?.title }}</h1>
           <button @click="router.back()" class="bg-[#ff00001a] p-2 rounded-xl self-start">
             <img src="../assets/icons/close.svg" alt="Fechar" />
           </button>
@@ -22,14 +39,7 @@ const router = useRouter()
             <img src="../assets/icons/description.svg" />
             Descrição
           </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nemo quaerat
-            necessitatibus a delectus excepturi cupiditate quae magni quidem impedit earum qui,
-            neque corrupti odio tenetur quod commodi voluptatem vero. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Placeat odit beatae in nisi rem ipsa quisquam. Quaerat
-            officiis optio incidunt odio. Quas mollitia autem officiis pariatur maxime deleniti
-            consequuntur repellat?
-          </p>
+          <p>{{ note?.description }}</p>
         </div>
 
         <div>
@@ -37,11 +47,16 @@ const router = useRouter()
             <img src="../assets/icons/link.svg" alt="" />
             Links
           </h3>
-          <ul class="flex flex-col gap-2 max-h-20 overflow-auto mb-3">
-            <li class="bg-[#06b6d424] p-2 underline text-cyan-500 rounded-xl">
-              <a href="https://github.com/kadymosantana">https://github.com/kadymosantana</a>
+          <ul v-if="note?.links.length" class="flex flex-col gap-2 max-h-20 overflow-auto mb-3">
+            <li
+              v-for="link in note?.links"
+              :key="link.id"
+              class="bg-[#06b6d424] p-2 underline text-cyan-500 rounded-xl"
+            >
+              <a :href="link.url">{{ link.url }}</a>
             </li>
           </ul>
+          <p v-else class="text-gray-600">Nenhum link adicionado.</p>
         </div>
 
         <div>
@@ -49,11 +64,10 @@ const router = useRouter()
             <img src="../assets/icons/tag.svg" />
             Tags
           </h3>
-          <ul class="flex flex-wrap gap-2 mb-3">
-            <Tag />
-            <Tag />
-            <Tag />
+          <ul v-if="note?.tags.length" class="flex flex-wrap gap-2 mb-3">
+            <Tag v-for="tag in note?.tags" :key="tag.id" :name="tag.name" />
           </ul>
+          <p v-else class="text-gray-600">Nenhum link adicionado.</p>
         </div>
       </div>
     </div>
