cd74fd7b3a97f337886d45bace99ed8f0982f1cd
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-14 20:03:27 +0000

    test: Display of submitted prop

---
+++ client/app/src/components/__tests__/Tag.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import Tag from '@/components/Tag.vue'
+
+describe('Tag', () => {
+  const wrapper = mount(Tag)
+
+  it('A tag exibe corretamente o nome passado como prop', async () => {
+    await wrapper.setProps({ name: 'vuejs' })
+    expect(wrapper.html()).toContain('vuejs')
+  })
+})
