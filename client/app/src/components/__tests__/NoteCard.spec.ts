03fd8e2fa123179c8cda5d30ae1a9976d52ea47e
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-17 04:05:25 +0000

    feat: Changing assertion method

--- client/app/src/components/__tests__/NoteCard.spec.ts
+++ client/app/src/components/__tests__/NoteCard.spec.ts
@@ -3,21 +3,21 @@ import { mount } from '@vue/test-utils'
 
 import NoteCard from '../NoteCard.vue'
 
-describe('NoteCard', () => {
-  const wrapper = mount(NoteCard, {
-    props: {
-      note: {
-        id: 1,
-        title: 'Fundamentos do Vue.js',
-        description: 'Entendendo o framework progressivo de JavaScript',
-        tags: ['vuejs', 'javascript']
-      }
+const wrapper = mount(NoteCard, {
+  props: {
+    note: {
+      id: 1,
+      title: 'Fundamentos do Vue.js',
+      description: 'Entendendo o framework progressivo de JavaScript',
+      tags: ['vuejs', 'javascript']
     }
-  })
+  }
+})
 
+describe('NoteCard', () => {
   it('The card displays the note information passed as a prop', async () => {
-    expect(wrapper.html()).toContain('Fundamentos do Vue.js')
-    expect(wrapper.html()).toContain('Entendendo o framework progressivo de JavaScript')
+    expect(wrapper.text()).toContain('Fundamentos do Vue.js')
+    expect(wrapper.text()).toContain('Entendendo o framework progressivo de JavaScript')
   })
 
   it('Card does not display tag list if it has no elements', async () => {
