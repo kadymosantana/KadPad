8678c143c592cc0f9e2f32c54992017ed7cda026
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-18 03:34:47 +0000

    test: v-model, props and add button

---
+++ client/app/src/components/__tests__/AddButton.spec.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+
+import AddButton from '@/components/AddButton.vue'
+
+const wrapper = shallowMount(AddButton, {
+  props: {
+    modelValue: 'initial text',
+    'onUpdate:modelValue': (e: Event) => wrapper.setProps({ modelValue: e }),
+    icon: 'add-link',
+    placeholder: 'Novo link'
+  }
+})
+
+describe('AddButton', () => {
+  const input = wrapper.find('input')
+
+  describe('v-model', () => {
+    it("The input emits the 'update:modelValue' event when typing", async () => {
+      await input.trigger('input')
+      expect(wrapper.emitted()).toHaveProperty('update:modelValue')
+    })
+
+    it("The 'modelValue' prop value is updated when the input emits its event", async () => {
+      await input.setValue('new text')
+      expect(wrapper.props('modelValue')).toBe('new text')
+    })
+  })
+
+  describe('Rendering dependent on passed props', () => {
+    it("The component renders the icon in the <img> tag according to the passed 'icon' prop", () => {
+      const img = wrapper.find('img')
+      expect(img.attributes('src')).toContain(wrapper.props('icon'))
+    })
+
+    it("The component renders the placeholder text in the input according to the passed 'placeholder' prop", () => {
+      expect(input.attributes('placeholder')).toEqual(wrapper.props('placeholder'))
+    })
+  })
+
+  describe('Add button', () => {
+    const addButton = wrapper.find('button')
+
+    it("The component emits the 'add' event when the add item button is clicked", async () => {
+      await addButton.trigger('click')
+      expect(wrapper.emitted()).toHaveProperty('add')
+    })
+  })
+})
