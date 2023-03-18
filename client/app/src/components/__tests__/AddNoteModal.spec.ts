90333e8045f323d78af234b80c0bf0984de749cd
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-03-18 03:35:33 +0000

    test: Link and tag lists

---
+++ client/app/src/components/__tests__/AddNoteModal.spec.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import AddNoteModal from '@/components/AddNoteModal.vue'
+import NoteItem from '@/components/NoteItem.vue'
+import AddButton from '@/components/AddButton.vue'
+
+const wrapper = mount(AddNoteModal)
+
+describe('AddNoteModal', () => {
+  const addLinkButton = wrapper.findAllComponents(AddButton)[0]
+  const addTagButton = wrapper.findAllComponents(AddButton)[1]
+
+  describe('Links list', () => {
+    it('Link list is not rendered if there are no links', () => {
+      wrapper.vm.links = []
+      expect(wrapper.find('.links').exists()).toBe(false)
+    })
+
+    it("The value of 'newLink' is added to the list of links when the 'add' event is emitted", async () => {
+      wrapper.vm.newLink = 'https://vuejs.org'
+      addLinkButton.vm.$emit('add')
+      expect(wrapper.vm.links.includes('https://vuejs.org')).toBe(true)
+    })
+
+    it("The value passed as an argument when issuing the 'remove' event is removed from the list of links", () => {
+      const linkItem = wrapper.findAllComponents(NoteItem)[0]
+      linkItem.vm.$emit('remove', linkItem.props('value')) // prop value: https://vuejs.org
+
+      expect(wrapper.vm.links.includes('https://vuejs.org')).toBe(false)
+    })
+  })
+
+  describe('Tags list', () => {
+    it('Tag list is not rendered if there are no tags', () => {
+      wrapper.vm.tags = []
+      expect(wrapper.find('.tags').exists()).toBe(false)
+    })
+
+    it("The value of 'newTag' is added to the list of tags when the 'add' event is emitted", () => {
+      wrapper.vm.newTag = 'vuejs'
+      addTagButton.vm.$emit('add')
+      expect(wrapper.vm.tags.includes('vuejs')).toBe(true)
+    })
+
+    it("The value passed as an argument when issuing the 'remove' event is removed from the tag list", () => {
+      const tagItem = wrapper.findAllComponents(NoteItem)[0]
+      tagItem.vm.$emit('remove', tagItem.props('value')) // prop value: vuejs
+
+      expect(wrapper.vm.tags.includes('vuejs')).toBe(false)
+    })
+  })
+})
