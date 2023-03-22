956e31af45e3079992f95343e1bf68d5c57b1a6e
Kádymo Santana <santanakadymo@gmail.com>
2023-03-22 04:34:09 +0000

    feat: Migration for creating users

---
+++ server/src/database/knex/migrations/20230321234614_createUsers.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.text("name");
+    table.text("email");
+    table.text("password");
+    table.text("avatar").nullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("users");
+}
