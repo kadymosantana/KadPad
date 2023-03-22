66f827e701c61683b0339464fb74cf733521d948
Kádymo Santana <santanakadymo@gmail.com>
2023-03-22 04:32:56 +0000

    feat: Adjusting responsiveness and mobile menu

--- client/app/src/components/Sidebar.vue
+++ client/app/src/components/Sidebar.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { Tag } from "@/types";
 
-import { ref, onMounted } from "vue";
+import { ref, onMounted, provide } from "vue";
+import { vOnClickOutside } from "@vueuse/components";
 
 import { api } from "@/services/api";
 import store from "@/store";
@@ -12,6 +13,8 @@ onMounted(() => {
   fetchTags();
 });
 
+const menu = ref(false);
+
 const tags = ref<Tag[]>([]);
 const fetchTags = async () => {
   const tagsData = await api.get("/tags");
@@ -21,6 +24,8 @@ const fetchTags = async () => {
 const tagAlreadySelected = (tagName: string) => store.selectedTags.includes(tagName);
 
 const selectTag = (tagName: string) => {
+  setTimeout(() => (menu.value = false), 200);
+
   if (tagAlreadySelected(tagName)) {
     const index = store.selectedTags.indexOf(tagName);
     store.selectedTags.splice(index, 1);
@@ -28,13 +33,27 @@ const selectTag = (tagName: string) => {
     store.selectedTags.push(tagName);
   }
 };
+
+provide("menu", menu);
 </script>
 
 <template>
   <aside
-    class="fixed grid h-screen w-80 grid-rows-[auto_1fr_auto] rounded-tr-3xl rounded-br-3xl bg-dark-700 p-4 shadow-lg"
+    v-on-click-outside="() => (menu = false)"
+    :class="{ 'z-10 grid w-80 grid-rows-[auto_1fr_auto]': menu, 'w-8': !menu }"
+    class="fixed h-screen rounded-tr-3xl rounded-br-3xl bg-dark-700 p-4 shadow-lg duration-500 sm:grid sm:w-80 sm:grid-rows-[auto_1fr_auto]"
   >
-    <div class="mt-3 flex items-center justify-center gap-4">
+    <button
+      @click="menu = !menu"
+      class="absolute top-[50%] bottom-[50%] right-[-1.5rem] z-10 flex max-h-max min-w-max items-center rounded-full bg-dark-600 p-2 shadow-xl sm:hidden"
+    >
+      <img :src="`src/assets/icons/${menu ? 'arrow-left' : 'arrow-right'}.svg`" alt="Menu" />
+    </button>
+
+    <div
+      :class="{ flex: menu, hidden: !menu }"
+      class="mt-3 items-center justify-center gap-4 sm:flex"
+    >
       <img
         width="34"
         src="https://user-images.githubusercontent.com/98963793/221386784-f28b7347-a757-47bc-951a-8622354c3e07.png"
@@ -43,7 +62,7 @@ const selectTag = (tagName: string) => {
       <span class="mt-1 text-2xl font-light text-cyan-500">KadPad</span>
     </div>
 
-    <div class="self-center">
+    <div :class="{ hidden: !menu }" class="self-center sm:block">
       <h3 class="mb-4 border-b border-solid border-dark-600 pb-2 text-xl">Minhas tags</h3>
       <ul
         class="flex max-h-60 flex-col gap-2 overflow-auto border-b border-solid border-dark-600 pb-4"
