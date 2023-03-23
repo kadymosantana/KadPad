9d4960a2ff5b41ea88984d5dcd88fa38f974717d
Kádymo Santana <santanakadymo@gmail.com>
2023-03-23 21:42:51 +0000

    feat: Loading spinner and expired token

--- client/app/src/components/NoteModal.vue
+++ client/app/src/components/NoteModal.vue
@@ -3,9 +3,12 @@ import type { Note } from "@/types";
 
 import { ref, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
+import { vOnClickOutside } from "@vueuse/components";
 import { useToast } from "vue-toastification";
+import BeatLoader from "vue-spinner/src/BeatLoader.vue";
 
 import { api } from "@/services/api";
+import store from "@/store";
 
 import Tag from "./Tag.vue";
 
@@ -19,8 +22,15 @@ onMounted(() => {
 
 const note = ref<Note | null>(null);
 const fetchNoteDetails = async () => {
-  const noteDetails = await api.get(`/notes/${route.params.id}`);
-  note.value = noteDetails.data;
+  try {
+    const noteDetails = await api.get(`/notes/${route.params.id}`);
+    note.value = noteDetails.data;
+  } catch (error: any) {
+    if (error.response.data.message === "Token inválido") {
+      store.authData = null;
+      toast.error("Sessão expirada. Faça login novamente.");
+    }
+  }
 };
 
 const activeDeleteButton = ref("delete");
@@ -32,66 +42,78 @@ const deleteNote = async () => {
 </script>
 
 <template>
-  <Transition name="modal">
-    <div class="modal-wrapper">
-      <div class="modal-container">
-        <header class="flex justify-between pb-4">
-          <h1 class="text-4xl font-bold">{{ note?.title }}</h1>
-          <button @click="router.back()" class="min-w-max self-start rounded-xl bg-[#ff00001a] p-2">
-            <img src="../assets/icons/close.svg" alt="Fechar" />
-          </button>
-        </header>
-
-        <div>
+  <div class="modal-wrapper p-5">
+    <div
+      v-on-click-outside="() => router.back()"
+      :class="{ 'items-center justify-center bg-transparent': !note, 'bg-dark-800': note }"
+      class="flex w-full flex-col overflow-hidden rounded-3xl sm:w-[700px] md:h-[550px] md:flex-row md:bg-dark-800"
+    >
+      <template v-if="note">
+        <div class="w-full border-r border-solid border-dark-600 p-4 md:w-96 md:p-7">
           <h3 class="subtitle">
             <img src="../assets/icons/description.svg" />
             Descrição
           </h3>
-          <p class="max-h-48 overflow-auto">{{ note?.description }}</p>
+          <p class="h-36 overflow-auto md:h-[480px]">{{ note?.description }}</p>
         </div>
 
-        <div>
-          <h3 class="subtitle">
-            <img src="../assets/icons/link.svg" alt="" />
-            Links
-          </h3>
-          <ul v-if="note?.links.length" class="mb-3 flex max-h-20 flex-col gap-2 overflow-auto">
-            <li
-              v-for="link in note?.links"
-              :key="link.id"
-              class="rounded-xl bg-[#06b6d424] p-2 text-cyan-500 underline"
+        <div class="grid max-h-full w-full flex-col gap-6 p-4 md:w-96 md:p-7">
+          <div>
+            <h3 class="subtitle">
+              <img src="../assets/icons/link.svg" alt="" />
+              Links
+            </h3>
+
+            <ul
+              v-if="note?.links.length"
+              class="mb-3 flex max-h-24 w-full flex-col gap-2 overflow-auto md:max-h-48"
             >
-              <a :href="link.url">{{ link.url }}</a>
-            </li>
-          </ul>
-          <p v-else class="text-gray-600">Nenhum link adicionado.</p>
-        </div>
+              <li
+                v-for="link in note?.links"
+                :key="link.id"
+                class="rounded-xl bg-[#06b6d424] p-2 text-cyan-500 underline"
+              >
+                <a :href="link.url">{{ link.url }}</a>
+              </li>
+            </ul>
+            <p v-else class="text-gray-600">Nenhum link adicionado.</p>
+          </div>
 
-        <div>
-          <h3 class="subtitle">
-            <img src="../assets/icons/tag.svg" />
-            Tags
-          </h3>
-          <ul v-if="note?.tags.length" class="mb-3 flex flex-wrap gap-2">
-            <Tag v-for="tag in note?.tags" :key="tag.id" :name="tag.name" />
-          </ul>
-          <p v-else class="text-gray-600">Nenhum link adicionado.</p>
+          <div>
+            <h3 class="subtitle">
+              <img src="../assets/icons/tag.svg" />
+              Tags
+            </h3>
+
+            <ul v-if="note?.tags.length" class="max-h-30 mb-3 flex flex-wrap gap-2">
+              <Tag v-for="tag in note?.tags" :key="tag.id" :name="tag.name" />
+            </ul>
+            <p v-else class="text-gray-600">Nenhum link adicionado.</p>
+          </div>
+
+          <div class="self-end overflow-hidden rounded-xl">
+            <button
+              v-if="activeDeleteButton === 'delete'"
+              @click="activeDeleteButton = 'confirm'"
+              class="delete w-full bg-[#ff00001a] p-3 text-red-700"
+            >
+              Excluir nota
+            </button>
+            <button
+              @click="deleteNote"
+              v-else
+              class="confirm w-full bg-[#fff7001a] p-3 text-yellow-600"
+            >
+              Confirmar exclusão
+            </button>
+          </div>
         </div>
-        <button
-          v-if="activeDeleteButton === 'delete'"
-          @click="activeDeleteButton = 'confirm'"
-          class="delete rounded-xl bg-[#ff00001a] p-3 text-red-700"
-        >
-          Excluir nota
-        </button>
-        <button
-          @click="deleteNote"
-          v-else
-          class="confirm rounded-xl bg-[#fff7001a] p-3 text-yellow-600"
-        >
-          Confirmar exclusão
-        </button>
-      </div>
+      </template>
+      <template v-else>
+        <div>
+          <BeatLoader :loading="true" height="80px" color="#06b6d4" />
+        </div>
+      </template>
     </div>
-  </Transition>
+  </div>
 </template>
