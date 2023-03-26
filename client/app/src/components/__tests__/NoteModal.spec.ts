339d74427b2ed6207bfd09602bee8427b1516af3
Kádymo Santana <santanakadymo@gmail.com>
2023-03-26 02:59:03 +0000

    feat: Updating tests that needed requests

--- client/app/src/components/__tests__/NoteModal.spec.ts
+++ client/app/src/components/__tests__/NoteModal.spec.ts
@@ -1,92 +1,45 @@
-import type { Note } from "@/types/index";
-
 import { describe, it, expect } from "vitest";
-import { flushPromises, mount } from "@vue/test-utils";
-import MockAdapter from "axios-mock-adapter";
-
-import { api } from "@/services/api";
-import store from "@/store";
+import { mount } from "@vue/test-utils";
 
 import NoteModal from "@/components/NoteModal.vue";
 import Tag from "@/components/Tag.vue";
 
-const wrapper = mount<any>(NoteModal);
-
-const mockData: Note = {
-  id: 1,
-  user_id: 1,
-  title: "Fundamentos do Vue.js",
-  description: "Entendendo o framework progressivo de JavaScript",
-  tags: [
-    {
+const wrapper = mount<any>(NoteModal, {
+  props: {
+    note: {
       id: 1,
-      note_id: 1,
       user_id: 1,
-      name: "vuejs"
-    },
-    {
-      id: 2,
-      note_id: 1,
-      user_id: 1,
-      name: "javascript"
+      title: "Fundamentos do Vue.js",
+      description: "Entendendo o framework progressivo de JavaScript",
+      tags: [
+        { id: 1, note_id: 1, user_id: 1, name: "vuejs" },
+        { id: 2, note_id: 1, user_id: 1, name: "javascript" }
+      ]
     }
-  ],
-  links: [{ id: 1, note_id: 1, url: "https://vuejs.org", created_at: "2023-02-22 12:00" }],
-  created_at: "2023-02-22 22:00",
-  updated_at: "2023-02-22 22:00"
-};
+  }
+});
 
 describe("NoteModal", async () => {
-  const mockAxios: MockAdapter = new MockAdapter(api);
+  describe("Props", () => {
+    it("The modal displays the note information passed as a prop", () => {
+      expect(wrapper.text()).toContain(wrapper.props("note").description);
 
-  mockAxios
-    .onGet("/notes/1", {
-      headers: { Authorization: `Bearer ${store.authData?.token}` }
-    })
-    .reply(200, mockData);
+      const tags = wrapper.findAllComponents(Tag);
+      const tagsNames = tags.map((tag) => tag.props("name"));
 
-  const result = await api.get("/notes/1");
-  wrapper.vm.note = result.data;
-
-  await flushPromises();
-
-  describe("Request verification", () => {
-    it("The request was made for the path /notes/1", () => {
-      expect(mockAxios.history.get[0].url).toEqual(`/notes/1`);
-    });
-
-    it("The result of the request is equal to the mock and the data received in the component match the result", async () => {
-      expect(result.data).toEqual(mockData);
-      expect(wrapper.vm.note).toEqual(result.data);
-    });
-
-    it("Title, description, tags and links information are displayed on the interface", () => {
-      expect(wrapper.text()).toContain(mockData.title);
-      expect(wrapper.text()).toContain(mockData.description);
-
-      expect(wrapper.text()).toContain(mockData.tags[0].name);
-      expect(wrapper.text()).toContain(mockData.tags[1].name);
-
-      expect(wrapper.text()).toContain(mockData.links[0].url);
-    });
-
-    it("Checking the 'name' prop value passed to the Tag component", () => {
-      const tagComponents = wrapper.findAllComponents(Tag);
-      const tagComponentsProps = tagComponents.map((tag) => tag.props("name"));
-
-      expect(tagComponents.length).toEqual(2);
-      expect(tagComponentsProps.includes(mockData.tags[0].name)).toEqual(true);
-      expect(tagComponentsProps.includes(mockData.tags[1].name)).toEqual(true);
+      expect(tagsNames.includes("vuejs")).toBe(true);
+      expect(tagsNames.includes("javascript")).toBe(true);
     });
   });
-  describe("Deleção da nota", () => {
-    it("Ao clicar no botão vermelho de deleção, o valor de 'activeDeleteButton' passa a ser 'confirm'", async () => {
+
+  describe("Note deletion", () => {
+    it("When clicking on the red delete button, the value of 'activeDeleteButton' becomes 'confirm'", async () => {
       const deleteButton = wrapper.find(".delete");
       await deleteButton.trigger("click");
       expect(wrapper.vm.activeDeleteButton).toEqual("confirm");
     });
 
-    it("Ao excluir a nota, o componente sai da interface", async () => {
+    it("When deleting the note, the component exits the interface", async () => {
       const confirmButton = wrapper.find(".confirm");
       wrapper.vm.activeDeleteButton = "confirm";
       await confirmButton.trigger("click");
