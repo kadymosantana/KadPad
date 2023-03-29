d12ca1f51ce24051b00a3967b81b675fe93e0902
Kádymo Santana <santanakadymo@gmail.com>
2023-03-29 00:52:06 +0000

    feat: Routes for creating and displaying notes

--- client/app/src/router/index.ts
+++ client/app/src/router/index.ts
@@ -1,22 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import store from "../store";
+import { authDataStore as authData } from "@/stores/authData";
 
 const Login = import("@/views/Login.vue");
-const Home = import("@/views/Home.vue");
+const Notes = import("@/views/Notes.vue");
 const Profile = import("@/views/Profile.vue");
+const NewNoteModal = import("@/components/NewNoteModal.vue");
+const NoteModal = import("@/components/NoteModal.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { name: "Login", path: "/", component: Login },
-    { name: "Home", path: "/home", component: Home },
+    {
+      name: "Notes",
+      path: "/notes",
+      component: Notes,
+      children: [
+        { name: "New Note", path: "new", component: NewNoteModal },
+        { name: "Note", path: ":id", component: NoteModal }
+      ]
+    },
     { name: "Profile", path: "/profile", component: Profile }
   ]
 });
 
 router.beforeEach((to, from, next) => {
-  if (!store.authData && to.name !== "Login") next({ name: "Login" });
+  if (!authData.token && to.name !== "Login") next({ name: "Login" });
   next();
 });
 
