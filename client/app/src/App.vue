88e9b8edfea54b02118e2524539c67832405cc14
Kádymo Santana <santanakadymo@gmail.com>
2023-03-29 00:31:49 +0000

    feat: Updating transition style

--- client/app/src/App.vue
+++ client/app/src/App.vue
@@ -5,21 +5,26 @@
 </template>
 
 <style>
-.modal-enter-from,
-.modal-leave-to {
-  opacity: 0;
-  transition: all 0.3s;
+.modal-enter-active,
+.modal-leave-active {
+  transition: 0.3s linear;
 }
 
 .modal-enter-from,
 .modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal,
+.modal-leave-to .modal {
+  opacity: 0;
   -webkit-transform: scale(0);
   transform: scale(0);
 }
 
-.modal-enter-active,
-.modal-leave-active {
-  transition: all 0.3s;
+.modal-enter-active .modal,
+.modal-leave-active .modal {
+  transition: transform 0.3s cubic-bezier(0.5, 0, 0.5, 1), opacity 0.3s linear;
 }
 
 *::-webkit-scrollbar {
