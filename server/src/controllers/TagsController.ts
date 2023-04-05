052ff2c11f106a18fa6f6ae7620fe0b94c73bddc
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-04-05 01:11:22 +0000

    fix: Error querying tags

--- server/src/controllers/TagsController.ts
+++ server/src/controllers/TagsController.ts
@@ -5,6 +5,7 @@ export default class TagsController {
   async index(req: Request, res: Response) {
     const user_id = req.user!.id;
     const userTags = await knex("tags")
+      .select("name", knex.raw("MAX(id) as id"))
       .where("user_id", user_id)
       .groupBy("name");
 
