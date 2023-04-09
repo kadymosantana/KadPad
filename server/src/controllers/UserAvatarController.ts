f4df5d352378f4817c3a448940335d502d0d34f4
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-04-09 23:40:29 +0000

    feat: Stopping using DiskStorage

--- server/src/controllers/UserAvatarController.ts
+++ server/src/controllers/UserAvatarController.ts
@@ -1,29 +1,25 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
+import type { User } from "../types";
+
 import knex from "../database/knex";
-import DiskStorage from "../providers/DiskStorage";
 import AppError from "../../utils/AppError";
-import { User } from "../types";
 
 export default class UserAvatarController {
   async update(req: Request, res: Response) {
     const user_id = req.user?.id;
-    const avatarFilename = req.file!.filename;
-
-    const diskStorage = new DiskStorage();
+    const avatarPath = req.file!.path;
 
     const user = await knex<User>("users").where("id", user_id).first();
-    if (!user)
+    if (!user) {
       throw new AppError(
         "Somente usuários autenticados podem alterar o avatar",
         401
       );
+    }
 
-    if (user.avatar) await diskStorage.deleteFile(user.avatar);
-    diskStorage.saveFile(avatarFilename);
-    user.avatar = avatarFilename;
-
+    user.avatar = avatarPath;
     await knex("users").update(user).where("id", user_id);
 
-    res.json(user);
+    return res.json(user);
   }
 }
