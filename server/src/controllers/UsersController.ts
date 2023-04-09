010d6257dc6a2384cee6d2279f20282606027053
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-04-09 23:41:27 +0000

    style: Removing comments

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -7,7 +7,6 @@ import AppError from "../../utils/AppError";
 const { hash, compare } = require("bcrypt");
 
 export default class UsersController {
-  // Criar um registo (POST)
   async create(req: Request, res: Response) {
     const { name, email, password } = req.body;
 
@@ -26,17 +25,14 @@ export default class UsersController {
     return res.status(201).json();
   }
 
-  // Atualizar um usuário (PUT)
   async update(req: Request, res: Response) {
     const { name, email, password, old_password } = req.body;
     const user_id = req.user!.id;
 
     const user = await knex<User>("users").where("id", user_id).first();
 
-    // Verificando se o usuário a ser atualizado existe e se o e-mail passado já está em uso
     if (!user) throw new AppError("Usuário não encontrado.");
 
-    // Lidando com a atualização de nome e e-mail
     if (email) {
       const userWithUpdatedEmail = await knex<User>("users")
         .where("email", email)
@@ -49,7 +45,6 @@ export default class UsersController {
     user.name = name ?? user.name;
     user.email = email ?? user.email;
 
-    // Lidando com a atualização de senha
     if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password);
 
@@ -66,7 +61,6 @@ export default class UsersController {
     } else if (password && !old_password)
       throw new AppError("Você precisa informar a sua senha antiga.");
 
-    // Atualizando usuário no banco de dados
     await knex("users")
       .where("id", user_id)
       .update({
