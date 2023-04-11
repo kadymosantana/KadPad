b4e211f30be5e88a11e0249fc94131879b8c9ea7
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-04-11 02:51:46 +0000

    feat: HTML history api middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,16 @@ import type { Request, Response, NextFunction } from "express";
 
 import express from "express";
 import "express-async-errors";
+
 import Routes from "./routes";
 import AppError from "../utils/AppError";
 
 const cors = require("cors");
+const history = require("connect-history-api-fallback");
 
 const app = express();
 app.use(express.json());
+app.use(history());
 app.use(cors({ origin: "https://kadpad.vercel.app" }));
 app.use(Routes);
 
