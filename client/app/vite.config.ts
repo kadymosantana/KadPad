07465508e39836f509c211892e8a6e88c0cd09dd
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-04-12 01:43:25 +0000

    build: Base public path

--- client/app/vite.config.ts
+++ client/app/vite.config.ts
@@ -6,7 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://kadpad.vercel.app",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
