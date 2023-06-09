8fb161a2e00f99ab35ddb89367d6087e9687d770
Kádymo Santana <98963793+kadymosantana@users.noreply.github.com>
2023-06-09 01:47:15 +0000

    feat: PWA plugin config

--- client/app/vite.config.ts
+++ client/app/vite.config.ts
@@ -1,11 +1,52 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa"
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
+      },
+      manifest: {
+        name: "KadPad",
+        short_name: "KadPad",
+        description: "KadPad é um aplicativo de bloco de notas que tem o objetivo de ajudar os usuários a organizarem suas anotações com suas tags e links úteis.",
+        theme_color: "#1F2125",
+        icons: [
+          {
+            src: "./pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "./pwa-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "./pwa-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "./pwa-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
